c9ab2a1c042faa287d117fb631374f9e28e3e4e5
LOVELYXNETWORK <168854548+LOVELYXNETWORK@users.noreply.github.com>
2024-05-26 10:54:44 +0530

    Update ping.py

--- pyrobot/plugins/tools/ping.py
+++ pyrobot/plugins/tools/ping.py
@@ -9,7 +9,7 @@ from pyrobot.helper_functions.cust_p_filters import f_onw_fliter
 # -- Constants -- #
 ALIVE = "ചത്തിട്ടില്ലാ..."
 HELP = "CAADAgAD6AkAAowucAABsFGHedLEzeUWBA"
-REPO = "User / Bot is available on GitHub:\n" "https://github.com/SpEcHiDe/PyroGramBot"
+REPO = "User / Bot is available on GitHub:\n" "https://github.com/SpEcHiDe/PYROXV2"
 # -- Constants End -- #
 
 
